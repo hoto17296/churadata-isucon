4a85099ed6ecd2eb4c8169ba61482bd79c00dca7
hoto <mail@hoto.me>
2022-06-03 23:27:16 +0900

    create vpc

--- churadata_isucon/churadata_isucon_stack.py
+++ churadata_isucon/churadata_isucon_stack.py
@@ -1,19 +1,17 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+from aws_cdk import Stack, aws_ec2 as ec2
 from constructs import Construct
 
-class ChuradataIsuconStack(Stack):
 
+class ChuradataIsuconStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ChuradataIsuconQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        self.vpc = ec2.Vpc(
+            self,
+            "ChuraDATA-ISUCON",
+            cidr="192.168.0.0/16",
+            max_azs=1,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(name="public", subnet_type=ec2.SubnetType.PUBLIC),
+            ],
+        )
